069b7ee7730a649ce9e31b7457ceb120757db357
Cheryln <cherylnbarber@gmail.com>
2017-04-13 12:03:20 -0700

    added knex config info added db connection info created animals database created a migration file

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+var hbs = require('hbs');
+ 
+
 
 var index = require('./routes/index');
 
@@ -11,14 +15,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
 
 app.use('/', index);
 
---
+++ db/migrations/20170413115951_created_animals_table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('animals', (table) => {
+    table.increments();
+    table.text('img_url').notNullable().defaultTo('');
+    table.string('name').notNullable().defaultTo('');
+    table.string('kind').notNullable();
+    table.integer('age').notNullable();
+    table.text('description');
+    table.boolean('is_adopted').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('animals');
+};
